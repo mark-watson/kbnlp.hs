ee3adca23b1eff7972180499d55d3372cfe9391e
Mark Watson <markw@markwatson.com>
2014-08-01 15:34:17 -0700

    -

--- PipeTool.hs
+++ PipeTool.hs
@@ -11,7 +11,22 @@ main = do
   s <- getLine
   let cats = bestCategories (splitWords s); 
       bestCat = if length cats > 0 then fst (cats !! 0) else ""; 
-      sum = summarize_s s; 
-      result = encode $ showJSON [bestCat, sum] in
-    putStrLn result
-         
+      sum = summarize_s s;
+      spwkc = splitWordsKeepCase s;
+      people = encode $ showJSON $ peopleNames spwkc;
+      countries = encode $ showJSON $ countryNames spwkc;
+      companies = encode $ showJSON $ companyNames spwkc; 
+      result = encode $ showJSON [bestCat, sum];
+      result2 = encode $ showJSON [people, countries, companies] in
+    do
+      putStr "category:\t"
+      putStrLn bestCat
+      putStr "summary:\t"
+      putStrLn sum
+      putStr "people:\t"
+      putStrLn people
+      putStr "companies:\t"
+      putStrLn companies
+      putStr "countries:\t"
+      putStrLn countries
+  main
